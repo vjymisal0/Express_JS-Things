70a5ca3a0aefad0369c545fd60c60ec490a13809
Vijay Misal <misalvijay153@gmail.com>
2023-12-24 22:45:54 +0530

    res.all

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const PATH = require('path');
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello world</h1>")
+})
+app.get('/contact', (req, res) => {
+    res.send('<h1>Contact page</h1>');
+})
+app.get('/files', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+
+})
+app.get('/jsonres', (req, res) => {
+    // res.json({
+    //     name: 'John',
+    //     age: 30
+    // })
+    res.send([{
+        firstName: 'vijay',
+        lastName: 'kumar'
+    }
+        , {
+        firstName: 'samantha',
+        lastName: 'shroff'
+    }])
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send([{ error: 'Page not found' }])
+})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} click to visit: http://localhost:${PORT}`)
+})
