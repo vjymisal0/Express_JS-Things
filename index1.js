ac51cd944761f120d9560a371cf75b5b8ec8dc01
Vijay Misal <misalvijay153@gmail.com>
2023-12-25 20:04:01 +0530

    static express

--- index1.js
+++ index1.js
@@ -4,8 +4,8 @@ const PORT = 3000
 const path = require('path')
 
 
-app.get('/', (req, res) => {
-    res.send("")
+app.get('/home', (req, res) => {
+    res.send(__dirname, "./example/index.html")
 })
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} click to visit: http://localhost:${PORT}`)
