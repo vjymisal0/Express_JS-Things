f6453977b724fd0925f29b1ada3c9f62e343aaa8
Vijay Misal <misalvijay153@gmail.com>
2023-12-26 20:59:32 +0530

    26 dec

---
+++ data.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        id: 1,
+        name: "Laptop",
+        price: 1000,
+        description: "This is a laptop",
+        Image: "https://cdn.thewirecutter.com/wp-content/media/2023/06/bestlaptops-2048px-9765.jpg"
+    },
+    {
+        id: 2,
+        name: "Mobile",
+        price: 500,
+        description: "This is a mobile",
+        Image: "https://images.unsplash.com/photo-1580910051074-3eb694886505?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBob25lfGVufDB8fDB8fHww"
+    },
+    {
+        id: 3,
+        name: "Smart Watch",
+        price: 200,
+        description: "This is a smart watch",
+        Image: "https://assets.ajio.com/medias/sys_master/root/20230516/JVIk/6463b536d55b7d0c63ba25da/-473Wx593H-4938378250-multi-MODEL.jpg"
+    }
+]
+module.exports = products;
--- index1.js
+++ index1.js
@@ -2,13 +2,44 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const path = require('path')
+const phone = require('./data')
 
-app.use(express.static('example'))
+// app.use(express.static('example'))
 
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, "./example/index.html"))
+app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, "./example/index.html"))
+    // res.json([{
+    //     name: 'John',
+    //     age: 30
+    // },
+    // {
+    //     name: 'Vijay',
+    //     age: 19
+    // }
+    // ])
+    // res.json(phone)
+    res.send("<h1>Home Page</h1><a href='/api/phone'>Phone</a>")
+
 })
-
+/*
+app.get('/api/phone/:phoneID', (req, res) => {
+    // const newItem = phone.map((item) => {
+    //     const { id, name, Image } = item
+    //     return { id, name, Image }
+    // }) //this is for filtering the data
+    const { phoneID } = req.params
+    console.log(req.params)
+    const singleProduct = phone.find((product) => product.id === Number(phoneID))   //this is for finding the data
+    if (!singleProduct) {
+        return res.status(404).send("Product does not exist")
+    }
+    res.json(singleProduct) //this is for sending the data
+})
+*/
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    res.send("Query Page")
+})
 
 
 app.listen(PORT, () => {
